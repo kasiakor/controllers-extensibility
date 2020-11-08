ee80e6f61b393a96a24e396f058b42fc9df685e0
kasia.kordys <kasia.kordys@gmail.com>
2020-11-08 20:45:56 +0000

    create custom controller factory

--- ControllerExtensibility/Global.asax.cs
+++ ControllerExtensibility/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ControllerExtensibility;
+using ControllerExtensibility.Infrastructure;
 
 namespace ControllerExtensibility
 {
@@ -13,6 +15,8 @@ namespace ControllerExtensibility
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ControllerBuilder.Current.SetControllerFactory(new CustomControllerFactory());
         }
     }
 }
