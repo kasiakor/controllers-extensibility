2d25f9d9b8306c9cf933ee5d830138e9897708d9
kasia.kordys <kasia.kordys@gmail.com>
2020-11-09 21:20:04 +0000

    create asynchronous controller

--- ControllerExtensibility/Controllers/RemoteDataController.cs
+++ ControllerExtensibility/Controllers/RemoteDataController.cs
@@ -1,16 +1,29 @@
 ﻿using ControllerExtensibility.Models;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 
 namespace ControllerExtensibility.Controllers
 {
     public class RemoteDataController : Controller
     {
         // GET: RemoteData
-        public ActionResult Data()
-        {
-            RemoteService service = new RemoteService();
-            string data = service.GetRemoteData();
-            return View((object)data);
+        //public ActionResult Data()
+        //{
+            //RemoteService service = new RemoteService();
+            //string data = service.GetRemoteData();
+            //Task object calls GetRemoteData()
+            public async Task<ActionResult> Data()
+            {
+                string data = await Task<string>.Factory.StartNew(() =>
+               {
+                   return new RemoteService().GetRemoteData();
+               });
+
+                return View((object)data);
+            }
+
+            //asynchrocous controller - async await keywords
+            
         }
     }
-}
+//}
