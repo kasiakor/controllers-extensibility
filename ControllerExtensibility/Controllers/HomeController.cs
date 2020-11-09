2050826b099dc8cfe058ddd42636dc7950a572b5
kasia.kordys <kasia.kordys@gmail.com>
2020-11-09 19:22:02 +0000

    apply method selection attribute to local request

---
+++ ControllerExtensibility/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ControllerExtensibility.Infrastructure;
+using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Home",
+                ActionName = "Index"
+            });
+        }
+
+        //The current request for action 'Index' on controller type 'HomeController' is ambiguous between the following action methods
+        //applying [Local] defined in Local Attribute.cs - method selection attribute 
+        [Local]
+        [ActionName("Index")]
+        public ActionResult LocalIndex()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Home",
+                ActionName = "Localndex"
+            });
+        }
+    }
+}
