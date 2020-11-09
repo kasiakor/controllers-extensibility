3dbefb418e2c76850dc2b58aa24f6465e0f6ed4d
kasia.kordys <kasia.kordys@gmail.com>
2020-11-09 20:29:36 +0000

    add session state attribute to controller class

---
+++ ControllerExtensibility/Controllers/FastController.cs
@@ -0,0 +1,28 @@
+﻿using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    [SessionState(System.Web.SessionState.SessionStateBehavior.Disabled)]
+    public class FastController : Controller
+    {
+        // GET: Fast
+
+        // when we apply SessionState attr to the controller session message will be lost, mvc fr will through exception - use ViewBag instead
+        //for value disabled HttpContext.Session is null
+        public ActionResult Index()
+        {
+            //Session["Message"] = "Hello";
+            ViewBag.Message = "Hello";
+            return View("Result", new Result
+            {
+                ControllerName = "Fast",
+                ActionName = "lndex"
+            });
+        }
+    }
+}
